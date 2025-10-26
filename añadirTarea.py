5a9923650d892cb52b9c08a23e4c675d3f8e7559
Rain <13darkdaniel13@gmail.com>
2025-10-26 17:47:15 -0600

    he añadido una nueva tarea!
    Added a comment indicating the task was added by Daniel Ramirez Notario.

---
+++ añadirTarea.py
@@ -0,0 +1,9 @@
+fun main() {
+    val tareas = mutableListOf<String>()
+    println("Agrega una tarea:")
+    val nuevaTarea = readln()
+    tareas.add(nuevaTarea)
+    println("Tarea agregada: $nuevaTarea")
+    println("Lista actual de tareas: $tareas")
+}
+//Tarea añadida por Daniel Ramirez Notario
